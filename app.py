1e904d27ddcfb67d6a1624928c995366a4dca4a4
vman0027 <vman0027@student.monash.edu>
2024-05-06 13:53:16 +1000

    wrote input iamge prompt function

--- app.py
+++ app.py
@@ -14,5 +14,43 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 model = genai.GenerativeModel("gemini-pro-vision")
 
 def get_gemini_response(input,image,prompt): 
-    response = ''
+    response = model.generate_content([input,image[0],prompt])
+    return response.text 
+
+def input_image_setup(uploaded_file): 
+    if uploaded_file is not None : 
+        bytes_data = uploaded_file.getvalue()
+
+        image_parts = [
+            {
+                "mime_type": uploaded_file.type , 
+                "data": bytes_data 
+            }
+        ]
+        return image_parts 
+    else : 
+        raise FileNotFoundError("No file uplaoded")
+
+
+st.set_page_config(page_title="Multi language Invoice Extractor")
+
+input = st.text_input("Input Prompt : " ,key="input")
+uploaded_file = st.file_uploader("Choose as image of the invoice ..." , 
+                                 type=["jpg","jpeg","png"])
+image = ''
+if uploaded_file is not None : 
+    image = Image.open(uploaded_file)
+    st.image(image,caption="uploaded Image" , use_column_width=True)
+
+submit = st.button("Tell me about the invoice")
+
+input_prompt = """
+You are an expert in understanding invoices. We will upload an image as invoice
+and you will have to answer any questions based on the uploaded invoice image.
+"""
+
+# if submit is clicked 
+if submit: 
+    image_data = ''
+
 
